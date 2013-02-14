b3e3a9b46f3c8ca3be1c4c4a709096ec752ff070
Song Gao <song@gao.io>
2013-02-14 11:46:22 -0600

    using go.pipelien

--- colorizer.go
+++ colorizer.go
@@ -1,31 +1,22 @@
 package main
 
-var colorizers = map[string]colorizer{
-	"build": getColorizer(build),
-}
+import (
+	"regexp"
+	"strings"
+)
 
-type colorizer func() (chan<- string, chan<- string, <-chan string, <-chan string)
-type lineProcessor func(string) string
+var buildRE, _ = regexp.Compile(`^(.*\.go)\:(\d*)\:(.*)\n?$`)
 
-func getColorizer(c lineProcessor) colorizer {
-	return func() (chan<- string, chan<- string, <-chan string, <-chan string) {
-		ch_stdout := make(chan string, 4)
-		ch_stderr := make(chan string, 4)
-		ch_colorout := make(chan string, 4)
-		ch_colorerr := make(chan string, 4)
-		h := func(ch_in <-chan string, ch_out chan<- string) {
-			ok := true
-			var line string
-			for ok {
-				line, ok = <-ch_in
-				if ok {
-					ch_out <- c(line)
-				}
-			}
-			close(ch_out)
+func build(in string) (out string) {
+	matches := buildRE.FindStringSubmatch(in)
+	if len(matches) > 0 {
+		out = sgrBoldBlue(matches[1]) + ":" + sgrBoldRed(matches[2]) + ":" + matches[3] + "\n"
+	} else {
+		if strings.HasSuffix(in, "\n") {
+			out = in
+		} else {
+			out = in + "\n"
 		}
-		go h(ch_stdout, ch_colorout)
-		go h(ch_stderr, ch_colorerr)
-		return ch_stdout, ch_stderr, ch_colorout, ch_colorerr
 	}
+	return
 }
--- main.go
+++ main.go
@@ -1,69 +1,25 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"io"
+	"github.com/songgao/go.pipeline"
 	"os"
-	"os/exec"
-	"sync"
 )
 
-func start(cmd *exec.Cmd, stdout chan<- string, stderr chan<- string) {
-	outio, _ := cmd.StdoutPipe()
-	errio, _ := cmd.StderrPipe()
-	out := bufio.NewReader(outio)
-	err := bufio.NewReader(errio)
-	cmd.Start()
-	h := func(ch chan<- string, in *bufio.Reader) {
-		var line string
-		var err error
-		for err == nil {
-			line, err = in.ReadString('\n')
-			if err == nil || err == io.EOF {
-				ch <- line
-			}
-		}
-		close(ch)
-	}
-	go h(stdout, out)
-	go h(stderr, err)
-}
-
-func findColorizer(command string) colorizer {
-	c, ok := colorizers[command]
-	if !ok {
-		return getColorizer(nocolor)
-	}
-	return c
+var colorizers = map[string]pipeline.LineProcessor{
+	"build": build,
 }
 
 func main() {
-	var stdout, stderr chan<- string
-	var colorout, colorerr <-chan string
+	var p *pipeline.Pipeline
 
 	if len(os.Args) == 1 {
-		stdout, stderr, colorout, colorerr = findColorizer("")()
-		start(exec.Command("go"), stdout, stderr)
+		p = pipeline.StartPipelineWithCommand("go")
 	} else {
-		stdout, stderr, colorout, colorerr = findColorizer(os.Args[1])()
-		start(exec.Command("go", os.Args[1:]...), stdout, stderr)
-	}
-
-	var wg sync.WaitGroup
-	h := func(ch <-chan string, out *os.File) {
-		ok := true
-		var line string
-		for ok {
-			line, ok = <-ch
-			if ok {
-				fmt.Fprint(out, line)
-			}
+		args := make([]interface{}, len(os.Args[1:]))
+		for i, v := range os.Args[1:] {
+			args[i] = v
 		}
-		wg.Done()
+		p = pipeline.StartPipelineWithCommand("go", args...).ChainLineProcessor(colorizers[os.Args[1]], nil)
 	}
-	wg.Add(2)
-	go h(colorout, os.Stdout)
-	go h(colorerr, os.Stderr)
-	wg.Wait()
+	p.PrintAll()
 }
